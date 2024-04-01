68fce347a99295851b94477156feb196eccf2a99
Francisco <franciscopassarelli7@gmail.com>
2024-04-01 19:53:55 -0300

    imagenes.

--- src/ManualParallax/pages/Home.jsx
+++ src/ManualParallax/pages/Home.jsx
@@ -31,7 +31,7 @@ const Home = () =>{
       <div className="card mb-3">  
  <div className="row g-0">
    <div className="col-md-4">
-     <img src="src/assets/fran.jpg" className="img-fluid rounded-start" alt="..."/>
+     <img src="https://avatars.githubusercontent.com/u/118229728?v=4" className="img-fluid rounded-start" alt="..."/>
    </div>
    <div className="col-md-8">
      <div className="card-body">
